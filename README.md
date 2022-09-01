4bf5214b2982054f7f1390001c1a0679a81c0baf
elliotimhoff <elliotgamer3@gmail.com>
2022-08-31 21:58:57 -0500

    updated read me and added additonal CL Args

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Info
-This is used to re establish any and all things that will get wiped bny a steam os update. When ever a package that will be wiped on an update is added to the system, a new file in the pkg folder with the required steps should be made.
+
+This is used to re-establish any and all things that will get wiped by a steamos update. When ever a package that will be wiped on an update is added to the system, a new file in the pkg folder with the required steps should be made.
+
+# Contributing
+
+Contributions can be done through requesting a package or with required steps in as an issue. Frequent contributors that have shown to be trusted will then be given access to open pull requests to help speed up the additon of packages.
--- reinstall-pkgs.sh
+++ reinstall-pkgs.sh
@@ -30,16 +30,66 @@ if [ "$1" = "--init" ]; then
     echo "Init script finished."
 fi
 
-# run all of the scripts in the pkg directory on a seprate fork
-for i in $( ls ./pkg ); do
-    # run the script in the background
-    bash ./pkg/$i&
-    # save the pid of the script toi an array
-    pids+=($!)
-done
-# loop through the pids array and wait for each process to finish
-for pid in ${pids[@]}; do
-    wait $pid
-done
-#print a message when all of the scripts have finished
-echo "All scripts have finished"
+#check if an --all option was passed in
+if ["$1" = "--all"]; then
+    # run all of the scripts in the pkg directory on a seprate fork
+    for i in $( ls ./pkg ); do
+        # run the script in the background
+        bash ./pkg/$i&
+        # save the pid of the script toi an array
+        pids+=($!)
+    done
+    # loop through the pids array and wait for each process to finish
+    for pid in ${pids[@]}; do
+        wait $pid
+    done
+    #print a message when all of the scripts have finished
+    echo "All scripts have finished"
+fi
+
+#check if the --list option was passed in
+if [ "$1" = "--list" ]; then
+    #print a list of the scripts in the pkg directory
+    echo "Scripts in the pkg directory:"
+    for i in $( ls ./pkg ); do
+        echo $i
+    done
+fi
+
+#check if the --these option was passed in
+#if so then run the scripts that were passed in
+if [ "$1" = "--these" ]; then
+    #loop through the arguments passed in
+    for i in "$@"; do
+        #check if the argument is a script in the pkg directory
+        if [ -f ./pkg/$i ]; then
+            #run the script in the background
+            bash ./pkg/$i&
+            #save the pid of the script to an array
+            pids+=($!)
+        fi
+    done
+    #loop through the pids array and wait for each process to finish
+    for pid in ${pids[@]}; do
+        wait $pid
+    done
+    #print a message when all of the scripts have finished
+    echo "All scripts have finished"
+fi
+
+#check if a --help option was passed in and if so, print the help message
+if [ "$1" = "--help" ]; then
+    echo "Usage: ./reinstall-pkgs.sh [OPTION]"
+    echo "Reinstall packages on a steamOS system."
+    echo " "
+    echo "Options:"
+    echo "  --all      install all packages in the pkg directory"
+    echo "  --list     list all packages in the pkg directory"
+    echo "  --these    install the packages from the pkg directory specified after the --these option"
+    echo "  --init     run the init script after an update to the system"
+    echo "  --test     run the test scripts in the test directory"
+    echo "  --help     display this help message"
+    echo " "
+    echo "Report bugs to <elliotgamer3@gmail.com>"
+    echo "Repository: <https://github.com/ElliotImhoff/Steam-Pacman-Tool.git>"
+fi
